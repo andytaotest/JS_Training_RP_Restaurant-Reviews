3ac051cfa473b43041cc2ab02da72855a7065409
andytao <andy_tao@rp.edu.sg>
2023-09-26 16:58:57 +0800

    CRUD + JWT

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Required packages
 const express = require('express');
 const mysql = require('mysql2/promise');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 // Express setup
@@ -19,8 +20,57 @@ const pool = mysql.createPool({
     queueLimit: 0
 });
 
+function generateToken(userId, role) {
+    return jwt.sign({ id: userId, role: role }, process.env.JWT_SECRET, { expiresIn: '1h' });
+}
+
+function verifyToken(req, res, next) {
+    // const token = req.headers.authorization; //JWT inside
+    const token = req.headers.authorization.split(" ")[1]; //split because using insomnia and there's a bearer in front
+    console.log(token);
+    if (!token) {
+        return res.status(403).send('No token provided');
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send('Failed to authenticate token');
+        }
+        req.userId = decoded.id;
+        req.role = decoded.role;
+        next();
+    });
+}
+
+// we assume verifyToken has been called on the request
+function checkRole(roles) {
+    return function(req,res,next) {
+        // includes function exists on array
+        // [1,2,3].includes(2) ==> true
+        // [1,2,3].includes(-99) ==> false
+        if (roles.includes(req.role)) {
+            next();
+        } else {
+            return res.status(403).send("Permission denied");
+        }
+    }
+}
 
 // RESTful routes
+
+// Login
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const [users] = await pool.execute('SELECT * FROM users WHERE username = ?', [username]);
+    const user = users[0];
+
+    if (user && user.password === password) {
+        const token = generateToken(user.id, user.role);
+        res.json({ token });
+    } else {
+        res.status(401).send('Invalid credentials');
+    }
+});
 
 // GET - Fetch all reviews
 app.get('/reviews', async (req, res) => {
@@ -28,7 +78,7 @@ app.get('/reviews', async (req, res) => {
     res.json(rows);
 });
 
-app.post('/reviews', async (req, res) => {
+app.post('/reviews', [verifyToken, checkRole(["admin", "editor"])], async (req, res) => {
     const query = "INSERT INTO reviews (restaurant_name, review_text, rating) VALUES (?, ?, ?)";
     await pool.execute(query, [
         req.body.restaurant_name, 
@@ -77,6 +127,7 @@ app.delete('/reviews/:id', async (req, res) => {
     res.send('Review deleted');
 });
 */
+
 // Start the server
 app.listen(port, () => {
     console.log(`Server running on <http://localhost>:${port}`);
